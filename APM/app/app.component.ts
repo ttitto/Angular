6e50382c72f2d63c949362792e89354bd6fac375
Todor Todorov <ttitto_prog@vfemail.net>
2017-08-14 21:42:47 +0300

    partially added product-list component

--- APM/app/app.component.ts
+++ APM/app/app.component.ts
@@ -3,7 +3,10 @@ import { Component } from '@angular/core';
 @Component({
     selector: 'pm-root',
     template: `
-        <div><h1>{{pageTitle}}</h1><div>My First Component</div></div>
+        <div>
+            <h1>{{pageTitle}}</h1>
+            <pm-products></pm-products>
+        </div>
     `
 })
 export class AppComponent {
